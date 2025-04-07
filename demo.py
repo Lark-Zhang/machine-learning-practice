b19d91bfc55796f5e6d471a04b592dfa126d2c0e
lark <zekzhang@mail.nwpu.edu.cn>
2025-04-07 11:33:28 +0800

    完整代码

--- demo.py
+++ demo.py
@@ -4,6 +4,7 @@ import numpy as np
 class Network(object):
 
     def __init__(self, sizes):
+        
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
